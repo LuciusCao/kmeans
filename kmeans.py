c52f34d21bc9b23fa51efc59c9e782c23dd2e05b
AgnusDei <lucius.cao@gmail.com>
2016-03-20 13:32:48 +0800

    tried to visualize the result...

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+# from matplotlib import pyplot as pp
 
 def init_centroids(data,k):
 	if not isinstance(data,list):
@@ -37,26 +38,38 @@ def new_centroids(data,centroids,result):
 def cost_function(data,centroids,result):
 	cost = 0
 	for i in range(len(centroids)):
-		d_cluster = np.sum(np.power((np.multiply(np.equal(result,i),data)-centroids[i]),2))
+		d_cluster = np.sum(np.power(np.multiply(np.equal(result,i),(data-centroids[i])),2))
 		cost += d_cluster
 	return cost
 
 # k = 5
-m = 500
-n = 10
+m = 200
+n = 2
 
 X = np.array([[random.random() for i in range(n)] for i in range(m)])
 X_norm = normalize(X)
+# x1 = X_norm[:,0]
+# x2 = X_norm[:,1]
 
-global_cost = []
+all_cost = []
 for k in range(1,10):
 	for j in range(10):
 		result = np.zeros((m,1))
 		centroids = init_centroids(X_norm,k)
-		local_cost = []
+		current_cost = []
 		for i in range(10):
 			result = assign_centroids(X_norm,centroids,result)
 			centroids = new_centroids(X_norm,centroids,result)
-			local_cost.append(cost_function(X_norm,centroids,result))
-	global_cost.append(min(local_cost))
-
+			# c1 = centroids[:,0]
+			# c2 = centroids[:,1]
+			# f = pp.figure()
+			# f.hold(True)
+			# pp.plot(x1,x2,'r+')
+			# pp.plot(c1,c2,'bo')
+			# f.hold(False)
+			# f.show()
+			# pp.pause(5)
+			# pp.close(f)
+			cost = cost_function(X_norm,centroids,result)
+		current_cost = cost
+	all_cost.append(current_cost)
